daa55aaa6836a1ff0500148213b58b0ad315fc03
97arkham <khairunarkham97@gmail.com>
2022-07-05 10:11:07 +0700

    20220705

---
+++ inputLRA.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://fmis.bpkp.go.id/rembangkab")
+
+driver.maximize_window()
+
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="hero"]/div/div/div[1]/div/div/button').click()
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="kduser"]').send_keys('anggaranbkd')
+driver.find_element_by_xpath('//*[@id="password"]').send_keys('kepegawaian')
+driver.find_element_by_xpath('//*[@id="btnSubmit"]').click()
+time.sleep(2)
+driver.get("https://rembangkab.fmis.id/akuntansi/skpd/saldo-awal/lra-lak")
+
+
